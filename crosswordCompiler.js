e5bc64a38fa29935a37b02b3b8b93d763b14915f
masfunaro <massimo.funaro@gmail.com>
2025-04-19 18:16:32 +0200

    Delete crosswordCompiler.js

--- crosswordCompiler.js
+++
@@ -1,6 +0,0 @@
-/* crosswordCompiler.js */
-// Funzioni principali per il rendering e l'interazione dei cruciverba
-function CrosswordCompiler(puzzleData, options, config) {
-    // Inizializzazione
-    console.log("CrosswordCompiler inizializzato", puzzleData);
-}
